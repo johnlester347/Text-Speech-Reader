331be853c1e6bec7415160ed8b6f5e4ce30488d3
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-06-26 17:35:57 +0700

    Styling the HTML content done

---
+++ js/index.js
@@ -0,0 +1,144 @@
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+const eychone = document.querySelector('h1');
+
+
+const data = [
+    {
+        image: './img/kaori2.jpg',
+        text: "Your Lie In April is the best anime"
+    },
+    {
+        image: './img/dp.jpg',
+        text: "Good Morning John Lester"
+    },
+    {
+        image: './img/coding.jpg',
+        text: "Coding is fun"
+    },
+    {
+        image: './img/dp.jpg',
+        text: "Good Afternoon Master John Lester"
+    },
+    {
+        image: './img/uiux.png',
+        text: "UI UX Design"
+    },
+    {
+        image: './img/Ui.jpg',
+        text: "User Interface"
+    },
+    {
+        image: './img/OOP.jpg',
+        text: "Object Oriented Programming"
+    },
+    {
+        image: './img/distractions.jpeg',
+        text: "Too many distractions"
+    },
+    {
+        image: './img/Hello.jpg',
+        text: 'Hello there'
+    },
+    {
+        image: './img/wrong.jpg',
+        text: 'Wrong Timing'
+    },
+    {
+        image: './img/LEZZ.jpg',
+        text: 'Lezzzz Do This'
+    },
+    {
+        image: './img/GG.jpg',
+        text: 'GG potaina'
+    }
+];
+
+data.forEach(createBox);
+
+// Create speech boxes
+function createBox(item) {
+    const box = document.createElement('div');
+
+    const { image, text } = item;
+
+    box.classList.add('box');
+
+    box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+    box.addEventListener('click', () => {
+        setTextMessage(text);
+        speakText();
+
+        // Add active effect
+        box.classList.add('active');
+        setTimeout(() => box.classList.remove('active'), 800);
+    });
+
+    main.appendChild(box);
+}
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Store voices
+let voices = [];
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+
+        voicesSelect.appendChild(option);
+    });
+}
+
+// Set text
+function setTextMessage(text) {
+    message.text = text;
+}
+
+// Speak text
+function speakText() {
+    speechSynthesis.speak(message);
+}
+
+// Set voice
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener('click', () =>
+    document.getElementById('text-box').classList.toggle('show')
+);
+
+// Close button
+closeBtn.addEventListener('click', () =>
+    document.getElementById('text-box').classList.remove('show')
+);
+
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
+
+// Read text button
+readBtn.addEventListener('click', () => {
+    setTextMessage(textarea.value);
+    speakText();
+});
+
+getVoices();
